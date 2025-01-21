5f4741b3a3c9becf388846f7526ee6f2728e1386
AEthier-AI <alex.ethie@gmail.com>
2025-01-21 12:04:24 -0500

    add script

--- Script1.R
+++ Script1.R
@@ -1,3 +1,36 @@
+# Script 1 utilisé pour tester des choses via RStudio, suivant mes lectures dans la littérature
+
+#Installation et lancement des packages####
+
+#Installation des packages
+
+install.packages("dplyr")
+install.packages("ggplot2")
+install.packages("readxl")
+
+
+
+
+
+
+
+
+#Chargement des packages
+
+library(dplyr)
+library(ggplot2)
+library(readxl)
+
+
+
+#CRÉATION DE DONNÉES####
+
+
+
+
+
+#TEST STATISTIQUE####
+
 
 
 
